ae7b94b878b6162599c5ac0a0f145f5c5e9f5bd7
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-08-08 09:24:44 +0100

    UPDATE

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,26 @@
-
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-
 namespace TestApi
 {
+    using System;
+    using System.IO;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.OpenApi.Models;
+
+    using Serilog;
+
     public class Startup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">CONFIG.</param>
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -20,6 +28,14 @@ namespace TestApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(loggingBuilder =>
+            loggingBuilder.AddSerilog(dispose: true));
+
+            var loggerServices = new ServiceCollection()
+               .AddLogging(builder =>
+               {
+                   builder.AddSerilog();
+               });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -34,6 +50,23 @@ namespace TestApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var callingAppName = env.ApplicationName;
+            var logfileFolder = "logs\\" + callingAppName;
+            bool isFolderCreated = Directory.Exists(logfileFolder);
+            if (!isFolderCreated)
+            {
+                Directory.CreateDirectory(logfileFolder);
+            }
+
+            Log.Logger = new LoggerConfiguration()
+              .Enrich.FromLogContext()
+              .WriteTo.File(logfileFolder + "\\log.txt", rollingInterval: RollingInterval.Day)
+              .CreateLogger();
+
+            var startTime = DateTimeOffset.UtcNow;
+
+            Log.Logger.Information("Started at {StartTime} and 0x{Hello:X} is hex of 42", startTime, 42);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
