2c7126348eaad530bc91ffa12a60a0d4e3b74772
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-08-08 10:38:14 +0100

    t

--- Startup.cs
+++ Startup.cs
@@ -51,10 +51,16 @@ namespace TestApi
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             var callingAppName = env.ApplicationName;
+            var logfileFolder = "logs";
+            bool isFolderCreated = Directory.Exists(logfileFolder);
+            if (!isFolderCreated)
+            {
+                Directory.CreateDirectory(logfileFolder);
+            }
 
             Log.Logger = new LoggerConfiguration()
               .Enrich.FromLogContext()
-              .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/.log"), rollingInterval: RollingInterval.Day)
+              .WriteTo.File(logfileFolder + "\\log.txt", rollingInterval: RollingInterval.Day)
               .CreateLogger();
 
             var startTime = DateTimeOffset.UtcNow;
